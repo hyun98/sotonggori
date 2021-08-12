4b3ffe3ef4a650dd5c81947b111e38754872395f
Henu <hyun0404woo@gmail.com>
2021-08-13 02:05:41 +0900

    urlname

--- sotongapp/migrations/0001_initial.py
+++ sotongapp/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=30)),
-                ('url_name', models.CharField(max_length=50)),
+                ('urlname', models.CharField(max_length=50)),
             ],
         ),
         migrations.CreateModel(
